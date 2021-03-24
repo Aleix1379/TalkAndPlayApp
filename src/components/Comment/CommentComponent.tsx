379cdbfc001abfaef68df64786a10d597f3a8b09
aleixmp@outlook.com <ok6oYN4S7Nq&>
2021-03-24 02:24:37 +0100

    Comments with style

--- src/components/Comment/CommentComponent.tsx
+++ src/components/Comment/CommentComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Comment} from "../../types/PostsTypes"
-import {Image, StyleSheet, View} from "react-native"
+import {Image, StyleProp, StyleSheet, TextStyle, View} from "react-native"
 import {Text, withTheme} from 'react-native-paper'
 import UserUtils from "../../utils/UserUtils"
 import {Theme} from "react-native-paper/lib/typescript/types"
@@ -15,6 +15,13 @@ interface CommentProperties {
     comment: Comment
     theme: Theme
     checkVisible: () => void
+}
+
+interface Item {
+    type?: '*' | '_' | '~'
+    value: string
+    start: number
+    end: number
 }
 
 const CommentComponent: React.FC<CommentProperties> = ({comment, theme, checkVisible}) => {
@@ -55,6 +62,71 @@ const CommentComponent: React.FC<CommentProperties> = ({comment, theme, checkVis
         options: {}
     })
 
+    const findSpecialCharacters = (specialCharacters: Item[], message: string, char: '*' | '_' | '~') => {
+        for (let i = 0; i < message.length; i++) {
+            if (message[i] === char) {
+                let index = message.indexOf(char, i + 1)
+                if (index >= 0) {
+                    specialCharacters.push({
+                        type: char,
+                        value: message.substring(i + 1, index),
+                        start: i + 1,
+                        end: index
+                    })
+                    i = index
+                }
+            }
+        }
+    }
+
+    const findNormalText = (specialCharacters: Item[], message: string) => {
+        const last = specialCharacters[specialCharacters.length - 1]?.end + 1
+        specialCharacters.forEach((sp, index) => {
+            specialCharacters.push({
+                value: message.substring(specialCharacters[index - 1]?.end + 1, sp.start - 1),
+                start: specialCharacters[index - 1] ? specialCharacters[index - 1].end + 1 : 0,
+                end: sp.start - 1
+            })
+        })
+
+        specialCharacters.push({
+            value: message.substring(last),
+            start: specialCharacters[specialCharacters.length - 1]?.end + 2,
+            end: message.length
+        })
+    }
+
+    const getStyleByCharacter = (char?: '*' | '_' | '~'): StyleProp<TextStyle> => {
+        const style: StyleProp<TextStyle> = {}
+        if (char === '*') {
+            style.fontWeight = 'bold'
+        }
+        if (char === '_') {
+            style.fontStyle = 'italic'
+        }
+        if (char === '~') {
+            style.textDecorationLine = 'line-through'
+        }
+        return style
+    }
+
+    const buildText = (text: string) => {
+        let specialCharacters: Item[] = []
+
+        findSpecialCharacters(specialCharacters, text, "*")
+        findSpecialCharacters(specialCharacters, text, "_")
+        findSpecialCharacters(specialCharacters, text, "~")
+        findNormalText(specialCharacters, text)
+
+        specialCharacters = specialCharacters.sort(((a, b) => a.start > b.start ? 1 : a.start < b.start ? -1 : 0))
+
+        console.log(specialCharacters)
+        console.log(text)
+        return (
+            specialCharacters.map(it => <Text key={it.start} style={getStyleByCharacter(it.type)}>{it.value}</Text>)
+        )
+    }
+
     return (
         <View style={styles.comment} onLayout={(_) => checkVisible()}>
             <View style={styles.details}>
@@ -69,7 +141,7 @@ const CommentComponent: React.FC<CommentProperties> = ({comment, theme, checkVis
                     onPress={() => console.log('open options...')}
                 />
             </View>
-            <Text>{comment.text}</Text>
+            <Text>{buildText(comment.text)}</Text>
         </View>
     )
 }
