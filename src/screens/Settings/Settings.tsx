6bf6cc177b6f0d14a81eed2e8f3555182e1b5e94
aleixmp@outlook.com <ok6oYN4S7Nq&>
2021-06-18 18:30:17 +0200

    De-active theme toggle

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {withTheme} from 'react-native-paper';
 import {Theme} from "react-native-paper/lib/typescript/types";
-import {DevSettings, StyleSheet, View} from "react-native";
+import {StyleSheet, View} from "react-native";
 import ButtonComponent from "../../components/ButtonComponent";
 import {connect, shallowEqual, useSelector} from "react-redux";
 import {logout} from "../../store/user/actions";
@@ -11,8 +11,6 @@ import UserService from "../../services/User";
 import {UserState} from "../../store/user/types";
 import {ApplicationState} from "../../store";
 import {setTheme} from "../../store/theme/actions";
-import LocalStorage from "../../utils/LocalStorage/LocalStorage";
-import ThemeToggleComponent from "../../components/ThemeToggleComponent";
 
 interface SettingsProperties {
     navigation: any,
@@ -21,16 +19,16 @@ interface SettingsProperties {
     setTheme: (theme: 'dark' | 'light') => void
 }
 
-const SettingsScreen: React.FC<SettingsProperties> = ({navigation, theme, logout, setTheme}) => {
+const SettingsScreen: React.FC<SettingsProperties> = ({navigation, theme, logout,}) => {
     const userService = new UserService()
     const [showDialog, setShowDialog] = useState(false)
     const user: UserState = useSelector((state: ApplicationState) => {
         return state.user
     }, shallowEqual)
 
-    const isDarkTheme: boolean = useSelector((state: ApplicationState) => {
-        return state.theme.isDarkTheme
-    }, shallowEqual)
+    // const isDarkTheme: boolean = useSelector((state: ApplicationState) => {
+    //     return state.theme.isDarkTheme
+    // }, shallowEqual)
 
     const styles = StyleSheet.create({
         settings: {
@@ -72,12 +70,12 @@ const SettingsScreen: React.FC<SettingsProperties> = ({navigation, theme, logout
         logout()
     }
 
-    const updateTheme = () => {
-        setTheme(isDarkTheme ? 'light' : 'dark')
-        LocalStorage.setTheme(isDarkTheme ? 'light' : 'dark')
-            .then(() => DevSettings.reload())
-            .catch(err => console.log(err))
-    }
+    // const updateTheme = () => {
+    //     setTheme(isDarkTheme ? 'light' : 'dark')
+    //     LocalStorage.setTheme(isDarkTheme ? 'light' : 'dark')
+    //         .then(() => DevSettings.reload())
+    //         .catch(err => console.log(err))
+    // }
 
     return (
         <>
@@ -94,7 +92,7 @@ const SettingsScreen: React.FC<SettingsProperties> = ({navigation, theme, logout
 
                 <View style={styles.actions}>
 
-                    <ThemeToggleComponent value={isDarkTheme} onPress={updateTheme} />
+                    {/*<ThemeToggleComponent value={isDarkTheme} onPress={updateTheme} />*/}
 
                     <ButtonComponent style={{...styles.action, backgroundColor: theme.colors.error}}
                                      label="Delete account"
