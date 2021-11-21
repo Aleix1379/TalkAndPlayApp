8707a9d5a17298328c9b181934661c7b218d48ef
Aleix <aleixmp1379@gmail.com>
2021-11-21 21:03:40 +0100

    Remove console.log

--- src/services/AdService/AdService.ts
+++ src/services/AdService/AdService.ts
@@ -7,12 +7,10 @@ class AdService {
     }
 
     public static getPostsListUnitAd() {
-        console.log(`get posts list: ${this.getUnitAd('ca-app-pub-3339437277990541/9618328669', 'ca-app-pub-3339437277990541/9857664440')}`)
         return this.getUnitAd('ca-app-pub-3339437277990541/9618328669', 'ca-app-pub-3339437277990541/9857664440')
     }
 
     public static getPostDetailUnitAd() {
-        console.log(`get post detail: ${this.getUnitAd('ca-app-pub-3339437277990541/6995180467', 'ca-app-pub-3339437277990541/1085729304')}`)
         return this.getUnitAd('ca-app-pub-3339437277990541/6995180467','ca-app-pub-3339437277990541/1085729304')
     }
 }
